1386bfb4be4d21ba4c044eaf69609440f2a917ea
Luís Machado <luis.kievel@gmail.com>
2024-01-24 17:54:54 -0300

    chore: change cors

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ DatabaseConfig.initialize();
 const app = express();
 app.use(cors({ origin: '*' })) 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use(express.json());
 app.use(express.static('./src/photos'));
 app.use(routes)
