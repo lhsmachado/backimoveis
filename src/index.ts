c065abee7a6bdaa4f53c4c17d2fecb39b8684f63
Luís Machado <luis.kievel@gmail.com>
2024-01-24 21:06:59 -0300

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,11 @@ const app = express();
 app.use(cors({ origin: '*' })) 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
     next();
   });
 
